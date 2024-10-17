1c078ca3d072d9409d2737a3006129bfc717e16e
kaycso <kaycsoliveira@hotmail.com>
2024-10-17 14:51:40 -0300

    add classes funcoes para criacao de usuarios e contas

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Deposito(Transacao):
         self._valor = valor
 
 
-class saque(Transacao):
+class Saque(Transacao):
     def __init__(self, valor):
         self._valor = valor
 
@@ -23,13 +23,13 @@ class Historico:
         pass
 
 
-class Conta:
-    def __init__(self, saldo, numero, agencia, cliente, historico):
-        self._saldo = saldo
+class Conta(ABC):
+    def __init__(self, numero, cliente):
+        self._saldo = 0
         self._numero = numero
-        self._agencia = agencia
+        self._agencia = "0001"
         self._cliente = cliente
-        self._historico = historico
+        self._historico = Historico()
     
     @property
     def saldo(self):
@@ -54,26 +54,41 @@ class Conta:
     @classmethod
     def nova_conta(cls, cliente, numero):
         return cls(cliente, numero)
-    
+
+    @abstractmethod
     def sacar(self, valor):
         pass
-    
+
+    @abstractmethod
     def depositar(self, valor):
         pass
 
 
 class ContaCorrente(Conta):
-    def __init__(self, limite, limite_saques, saldo, numero, agencia, cliente, historico):
-        super().__init__(saldo, numero, agencia, cliente, historico)
+    def __init__(self, numero, cliente, limite=500, limite_saques=3):
+        super().__init__(numero, cliente)
         self._limite = limite
         self._limite_saques = limite_saques
-        
+    
+    def sacar(self, valor):
+        pass
 
-class Cliente:
+    def depositar(self, valor):
+        pass
+
+class Cliente(ABC):
     def __init__(self, endereco):
         self._endereco = endereco
         self._contas = []
-    
+
+    @property
+    def contas(self):
+        return self._contas
+
+    @property
+    def endereco(self):
+        return self._endereco
+
     def realizar_transacao(self, conta, transacao):
         transacao.registrar(conta)
 
@@ -88,6 +103,22 @@ class PessoaFisica(Cliente):
         self._nome = nome
         self._data_nascimento = data_nascimento
 
+    def __str__(self):
+        mensagem = f"\nCliente:\t\t{self.nome}\nCPF:\t\t\t{self.cpf}\nData de Nascimento:\t{self.data_nascimento}\nEndereço:\t\t{self.endereco}\nContas:\t\t\t{len(self.contas)}\n"
+        return mensagem
+
+    @property
+    def cpf(self):
+        return self._cpf
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def data_nascimento(self):
+        return self._data_nascimento
+
 
 def menu():
     menu = """
@@ -98,28 +129,105 @@ def menu():
     [nc]\tNova Conta
     [lc]\tListar Contas
     [nu]\tNovo Usuário
+    [lu]\tListar Usuários
     [q]\tSair
-    
+
     =>"""
     return input(textwrap.dedent(menu))
 
 
-def main():
-    LIMITE_SAQUES = 3
-    AGENCIA = "0001"
+def criar_usuario(usuarios):
+    cpf = input("Informe o CPF (somente números): ")
+    usuario = filtrar_usuario(cpf, usuarios)
+    
+    if usuario:
+        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        return
+    
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
 
-    saldo = 0
-    limite = 500
-    extrato = ""
-    numero_saques = 0
+    usuario = PessoaFisica(cpf, nome, data_nascimento, endereco)
+
+    usuarios.append(usuario)
+    
+    print("=== Usuário criado com sucesso! ===")
+    
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario.cpf == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+
+def listar_usuarios(usuarios):
+    print("========== Usuários Cadastrados ==========")
+    for usuario in usuarios:
+        print(usuario)
+    print("==========================================")
+
+
+def criar_conta(contas, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+    numero = len(contas) + 1
+
+    if usuario:
+        nova_conta = ContaCorrente(numero, usuario)
+        contas.append(nova_conta)
+
+        usuario.adicionar_conta(nova_conta)
+
+        print("\n=== Conta criada com sucesso! ===")
+        return
+
+    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
+
+
+def mostrar_conta(contas):
+    for conta in contas:
+        linha = f"""
+            Agência:\t{conta.agencia}
+            C/C:\t\t{conta.numero}
+            Titular:\t{conta.cliente.nome}
+        """
+        print("=" * 100)
+        print(textwrap.dedent(linha))
+
+
+def listar_contas(contas, usuarios):
+    opcao = input("\nDeseja filtrar por Cliente? (S/N)\n").lower()
+
+    if opcao == "s":
+        cpf = input("\nInsira o CPF do usuário: ")
+        usuario = filtrar_usuario(cpf, usuarios)
+
+        if usuario:
+            mostrar_conta(usuario.contas)
+        else:
+            print("@@@ Usuário não Encontrado! @@@")
+    else:
+        mostrar_conta(contas)
+
+
+def main():
     usuarios = []
     contas = []
 
     while True:
         opcao = menu()
         
-        if opcao == "nu":
-            pass  
+        if opcao == "nc":
+            criar_conta(contas, usuarios)
+
+        elif opcao == "lc":
+            listar_contas(contas, usuarios)
+
+        elif opcao == "nu":
+            criar_usuario(usuarios)
+
+        elif opcao == "lu":
+            listar_usuarios(usuarios)
             
         elif opcao == "q":
             break
